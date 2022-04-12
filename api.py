67590214765e997b7b8afd7968e2d4dc58fac70b
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-12 18:50:27 -0500

    Update api.py

--- api.py
+++ api.py
@@ -2,19 +2,23 @@ import requests
 import html
 import json
 
+class UserAPI:
+  def __init__(self, user):
+    self.user = user
+    self.url = html.escape(f"https://api.github.com/users/{user}")
+    request = requests.get(self.url)
+    request.raise_for_status()
+    self.resp = json.loads(request.text)
+  def getRepo(self, repoName):
+    return RepoAPI(self.user, repoName)
+
 class RepoAPI:
   def __init__(self, user, repo):
     self.user = user
     self.repo = repo
     self.url = html.escape(f"https://api.github.com/repos/{user}/{repo}")
-    self.data = requests.get(self.url).text
+    request = requests.get(self.url)
+    request.raise_for_status()
+    self.resp = json.loads(request.text)
   def getUser(self):
     return UserAPI(self.user)
-
-class UserAPI:
-  def __init__(self, user):
-    self.user = user
-    self.url = html.escape(f"https://api.github.com/users/{user}")
-    self.data = requests.get(self.url).text
-  def getRepo(self, repoName):
-    return RepoAPI(self.user, repoName)
